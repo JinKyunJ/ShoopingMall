93a2dde913b31cc5cdb08a42db591f4720e7111b
JinkyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-06-11 16:55:51 +0900

    로그인 JS 업데이트

---
+++ src/FE/Login/Login.js
@@ -0,0 +1,43 @@
+/** X 버튼 클릭 시 모달 창 닫기 */
+const OnCloseButton = document.querySelector('.head-x-button');
+const OnModalContainer = document.querySelector('.container');
+
+OnCloseButton.addEventListener('click', () => {
+    OnModalContainer.style.display = 'none';
+});
+
+/** 로그인 버튼 클릭 시 서버에 사용자가 입력한 id, password 전송 */
+const OnLonginButton = document.querySelector('.login-button');
+const idInput = document.getElementById('id-input');
+const passwordInput = document.getElementById('password-input');
+
+OnLonginButton.addEventListener('submit', async (event) => {
+    event.preventDefault(); /** 기본 폼 제출 방지 */
+
+    const id = (idInput = idInput.value);
+    const password = passwordInput.value;
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            body: JSON.stringify({ id, password }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            /** 서버 응답 처리 */
+            const data = await response.json();
+            console.log('로그인 성공:', data);
+        } else {
+            /** 오류 처리 */
+            const errorData = await response.json();
+            console.error('로그인 실패:', errorData);
+            alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+        }
+    } catch (error) {
+        console.error('오류:', error);
+        alert('오류가 발생했습니다. 나중에 다시 시도해주세요.');
+    }
+});
