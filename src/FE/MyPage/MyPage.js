c364fbb7d63c759c4e3c61c400cd2bc671457de0
JinkyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-06-14 18:27:56 +0900

    마이페이지 html, CSS, JS: 불필요한 코드 삭제, 수정, 오류처리 및 로컬 스토리지에서 JWT 토근제거 업데이트

--- src/FE/MyPage/MyPage.js
+++ src/FE/MyPage/MyPage.js
@@ -18,7 +18,11 @@ const fetchAndNavigate = async (url, event) => {
         if (response.ok) {
             window.location.href = url;
         } else {
+            /** 오류 처리 */
+            const errorData = await response.json();
+            /** 사용자 알림 + 오류 추적 및 실행 중단 throw new Error 추가 */
             alert('페이지 이동에 실패했습니다. 나중에 다시 시도해주세요.');
+            throw new Error(errorData.message || '페이지 이동 실패');
         }
     } catch (error) {
         console.error('오류:', error);
@@ -40,16 +44,20 @@ const fetchUserName = async () => {
             const userNameElement = document.getElementById('user-name'); /** 'user-name' ID를 가진 HTML요소 */
             userNameElement.textContent = `${data.name}님`; /** 요소의 텍스트 내용을 `${data.name}님`으로 변경 */
         } else {
-            console.error('사용자 정보를 가져오는 데 실패했습니다.');
+            /** 오류 처리 */
+            const errorData = await response.json();
+            alert('사용자 정보를 가져오는 데 실패했습니다.');
+            throw new Error(errorData.message || '사용자 정보 가져오기 실패');
         }
     } catch (error) {
         console.error('오류:', error);
+        alert('오류가 발생했습니다. 나중에 다시 시도해주세요.');
     }
 };
 /** 사용자 이름 가져오기 함수 호출 */
 fetchUserName();
 
-/** 장바구니 버튼 클릭 시 장바구니 페이지로 이동 */
+/** cart 버튼 클릭 시 장바구니 페이지로 이동 */
 const OnCartButton = document.querySelector('.head-cart-button');
 OnCartButton.addEventListener('click', () => {
     window.location.href = '/cart';
@@ -74,7 +82,7 @@ setEventListener('.menu-info-section button', 'click', async (event) => {
 });
 
 /** 로그아웃 버튼 클릭 시 로그아웃 처리 후 홈 페이지로 이동 */
-const onLogoutButton = document.querySelector('.logout-button button');
+const onLogoutButton = document.querySelector('.logout-button');
 onLogoutButton.addEventListener('click', async (event) => {
     event.preventDefault(); /** 기본 동작 막기 */
     try {
@@ -85,9 +93,14 @@ onLogoutButton.addEventListener('click', async (event) => {
             }
         });
         if (response.ok) {
+            // 로컬 스토리지에서 JWT 토근 제거
+            localStorage.removeItem('jwtToken');
             window.location.href = '/home';
         } else {
+            /** 오류 처리 */
+            const errorData = await response.json();
             alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
+            throw new Error(errorData.message || '로그아웃 실패');
         }
     } catch (error) {
         console.error('오류:', error);
