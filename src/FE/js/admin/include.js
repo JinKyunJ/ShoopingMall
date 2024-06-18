d77b50d32841f18efb5a7666598df2337dd81026
ParkBoeun <supbe0726@naver.com>
2024-06-18 11:29:52 +0900

    관리자 페이지 작업

--- src/FE/js/admin/include.js
+++ src/FE/js/admin/include.js
@@ -7,8 +7,14 @@ function loadHTML(selector, url) {
     .catch((error) => console.error("Error loading the HTML:", error));
 }
 
-// 페이지 로드 시 header와 footer를 불러옴
+// 페이지 로드 시 header와 nav 불러옴
 document.addEventListener("DOMContentLoaded", () => {
-  loadHTML("header", "/src/FE/Admin/header.html");
-  loadHTML("nav", "/src/FE/Admin/menu.html");
+  loadHTML("header", "/src/FE/Admin/layout/header.html");
+  loadHTML("nav", "/src/FE/Admin/layout/menu.html");
+
+  // nav 로드가 완료된 후 admin.js의 로직을 실행
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "../js/admin/admin.js";
+  document.body.appendChild(script);
 });
